01c7917afd34bd9f2e7b9efec92e1297be3af138
Slater-1 <c.slater90@gmail.com>
2020-06-19 21:22:27 -0500

    Switch Hazel to Static Lib from Dynamic Lib
    Linking errors occured with ImGui in the Sandbox executable. Sandbox
    links Hazel, Hazel links ImGui. However, Hazel did not import the
    necessary symbols from ImGui that Sandbox will need.
    
    Sandbox will be using ImGui::Begin, ImGui::Text, and ImGui::End(). It
    does not make sense for Hazel to use these functions. An alternative
    that was not chosen was a DEF (*.def) file that exported those function
    symbols.
    
    Ultimately, to avoid using a DEF file and other arhitectural reasons
    (speed, hot swapping occurs on game code not engine code, stricter
    warnings on dlls), Hazel has been switched to a static library.

--- premake5.lua
+++ premake5.lua
@@ -24,9 +24,10 @@ include "HazelClone/vendor/imgui"
 
 project "HazelClone"
 	location "HazelClone"
-	kind "SharedLib"
+	kind "StaticLib"
 	language "C++"
-	staticruntime "off"
+	cppdialect "C++17"
+	staticruntime "on"
 	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("obj/" .. outputdir .. "/%{prj.name}")
@@ -61,7 +62,6 @@ project "HazelClone"
 	}
 
 	filter "system:windows"
-		cppdialect "C++17"
 		systemversion "latest"
 		
 		defines
@@ -71,31 +71,27 @@ project "HazelClone"
 			"GLFW_INCLUDE_NONE"
 		}
 		
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
-		}
-		
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
 		runtime "Debug"
-		symbols "On"
+		symbols "on"
 		
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 		
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
-	staticruntime "off"
+	cppdialect "C++17"
+	staticruntime "on"
 	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("obj/" .. outputdir .. "/%{prj.name}")
@@ -110,6 +106,7 @@ project "Sandbox"
 	{
 		"HazelClone/vendor/spdlog/include",
 		"HazelClone/src",
+		"HazelClone/vendor",
 		"%{IncludeDir.glm}"
 	}
 	
@@ -119,7 +116,6 @@ project "Sandbox"
 	}
 	
 	filter "system:windows"
-		cppdialect "C++17"
 		systemversion "latest"
 		
 		defines
@@ -130,14 +126,14 @@ project "Sandbox"
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
 		runtime "Debug"
-		symbols "On"
+		symbols "on"
 		
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
