0dbb46bf89bb6f20deda6396668f5d3aba963678
Slater-1 <c.slater90@gmail.com>
2020-08-14 20:17:42 -0500

    Add VertexArray
    Vertex arrays keep a reference to a vertex buffer and an index buffer.
    In OpenGL, this allows for a reduction in the number of duplicate binds
    when rendering multiple vertex buffers. However, this construct does not
    appear to be used in DirectX or Vulkan and does not seem to be well
    liked.
    
    End of video: Vertex Arrays

--- HazelClone/src/Hazel/Application.h
+++ HazelClone/src/Hazel/Application.h
@@ -11,6 +11,7 @@
 
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
 
 namespace Hazel {
 
@@ -40,11 +41,11 @@ namespace Hazel {
 
 		static Application* s_Instance;
 
-		unsigned int m_VertexArray;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
 
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	};
 
 	// To be defined in a CLIENT
