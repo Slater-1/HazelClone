312bb0ae63bb9de5ad8bc02d3db87cfaed351a3c
Slater-1 <c.slater90@gmail.com>
2020-08-22 16:53:15 -0500

    Add orthographic camera
    A camera can now be set by the renderer's BeginScene() call. The
    renderer will use the camera's ViewProjectionMatrix to draw the scene in
    the position of the camera.
    
    The orthographic camera's ViewProjectionMatrix is used to render 2D
    scenes whereas the yet to be added projection camera's
    ViewProjectionMatrix is used to render 3D scenes.

--- HazelClone/src/Hazel/Application.cpp
+++ HazelClone/src/Hazel/Application.cpp
@@ -15,6 +15,7 @@ namespace Hazel {
 		Application* Application::s_Instance = nullptr;
 		
 		Application::Application()
+			: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 		{
 			HZ_CORE_ASSERT(!s_Instance, "Application already exists!")
 			s_Instance = this;
@@ -54,10 +55,10 @@ namespace Hazel {
 			m_SquareVA.reset(VertexArray::Create());
 
 			float squareVertices[3 * 4] = {
-				-0.75f, -0.5f, 0.0f,
-				0.75f,  -0.5f, 0.0f,
-				0.75f,   0.5f, 0.0f,
-				-0.75f,  0.5f, 0.0f
+				-0.75f, -0.75f, 0.0f,
+				0.75f,  -0.75f, 0.0f,
+				0.75f,   0.75f, 0.0f,
+				-0.75f,  0.75f, 0.0f
 			};
 
 			std::shared_ptr<VertexBuffer> squareVB;
@@ -80,6 +81,8 @@ namespace Hazel {
 				layout(location = 0) in vec3 a_Position;
 				layout(location = 1) in vec4 a_Color;
 
+				uniform mat4 u_ViewProjection;
+
 				out vec3 v_Position;
 				out vec4 v_Color;
 				
@@ -87,7 +90,7 @@ namespace Hazel {
 				{
 					v_Position = a_Position;
 					v_Color = a_Color;
-					gl_Position = vec4(a_Position, 1.0);
+					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 				}
 			)";
 
@@ -114,12 +117,14 @@ namespace Hazel {
 
 				layout(location = 0) in vec3 a_Position;
 
+				uniform mat4 u_ViewProjection;
+
 				out vec3 v_Position;
 				
 				void main()
 				{
 					v_Position = a_Position;
-					gl_Position = vec4(a_Position, 1.0);
+					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 				}
 			)";
 
@@ -179,13 +184,13 @@ namespace Hazel {
 				RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 				RenderCommand::Clear();
 
-				Renderer::BeginScene();
+				m_Camera.SetPosition({ 0.5f, 0.5f, 0.0f });
+				m_Camera.SetRotation(45.0f);
 
-				m_BlueShader->Bind();
-				Renderer::Submit(m_SquareVA);
+				Renderer::BeginScene(m_Camera);
 
-				m_Shader->Bind();
-				Renderer::Submit(m_VertexArray);
+				Renderer::Submit(m_BlueShader, m_SquareVA);
+				Renderer::Submit(m_Shader, m_VertexArray);
 
 				Renderer::EndScene();
 
