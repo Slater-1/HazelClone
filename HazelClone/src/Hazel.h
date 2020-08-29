da1e6989d70e7594ccfb14d187c76222d94900de
Slater-1 <c.slater90@gmail.com>
2020-08-28 20:01:25 -0500

    Refactor Application class
    Application is largely used for setting layers and dispatching events.
    Prior to this commit, a large amount of renderer related logic existed
    inside of it for early testing. This logic has now been moved into
    SandboxApp.

--- HazelClone/src/Hazel.h
+++ HazelClone/src/Hazel.h
@@ -5,7 +5,19 @@
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
+
+// ----Renderer----------------------
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+// -----------------------------------
 
 // ----Entry Point--------------------
 #include "Hazel/EntryPoint.h"
--- HazelClone/src/Hazel/Application.h
+++ HazelClone/src/Hazel/Application.h
@@ -9,12 +9,6 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
-#include "Hazel/Renderer/Shader.h"
-#include "Hazel/Renderer/Buffer.h"
-#include "Hazel/Renderer/VertexArray.h"
-
-#include "Hazel/Renderer/OrthographicCamera.h"
-
 namespace Hazel {
 
 	class HAZEL_API Application
@@ -42,14 +36,6 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		static Application* s_Instance;
-
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SquareVA;
-
-		OrthographicCamera m_Camera;
 	};
 
 	// To be defined in a CLIENT
